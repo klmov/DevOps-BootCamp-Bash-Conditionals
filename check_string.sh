4aeb8eb2e17652fb8f3174869e1152aba80f03d1
Ranjit Kumar <95127402+ravikpt@users.noreply.github.com>
2022-12-23 07:22:11 +0000

    Adding my script

--- check_string.sh
+++ check_string.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-# Place your code here
+# Script for counting Words, numbers and symbols in a string
+
+string=$@
+
+# Word count
+
+words=$(echo -n "$string" | tr -cd '[:alpha:]' | wc -c)
+
+# numbers count
+
+numbers=$(echo -n "$string" | tr -cd '[:digit:]' | wc -c)
+
+# symbols count
+
+symbols=$(echo -n "$string" | tr -cd '[:punct:]' | wc -c)
+
+# output
+
+echo -n "Letters: $words,"
+echo -n "Numbers: $numbers,"
+echo "Symbols: $symbols."
+
