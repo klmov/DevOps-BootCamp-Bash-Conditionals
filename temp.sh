4e4c297fc7fbaa4029f4766edf86a25daa1f43cb
Ranjit Kumar <95127402+ravikpt@users.noreply.github.com>
2022-12-24 16:06:20 +0400

    Adding code for temp check

--- temp.sh
+++ temp.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-# Place your code here
+input=$1
+temp=0
+if [[ ${input:((${#input}-1)):1} = "C" ]]; then
+    ((temp=${input:0:((${#input}-1))}+273))
+    echo $temp"K"
+elif [[ ${input:((${#input}-1)):1} = "K" ]]; then
+    ((temp=${input:0:((${#input}-1))}-273))
+     echo $temp"C"
+fi
+
