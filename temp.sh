bef0f57ca2cfe5e4be3508ce21d6223e7a173a7f
Ranjit Kumar <95127402+ravikpt@users.noreply.github.com>
2022-12-26 23:44:22 +0400

    some changes

--- temp.sh
+++ temp.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-input=$1
-temp=0
-if [[ ${input:((${#input}-1)):1} = "C" ]]; then
-    ((temp=${input:0:((${#input}-1))}+273))
-    echo $temp"K"
-elif [[ ${input:((${#input}-1)):1} = "K" ]]; then
-    ((temp=${input:0:((${#input}-1))}-273))
-     echo $temp"C"
-fi
+#!/bin/bash
 
+input=$1
+res=0
+if [[ ${input:((${#input}-1)):1} = "C" ]]; then
+    ((res=${input:0:((${#input}-1))}+273))
+    echo $res"K"
+elif [[ ${input:((${#input}-1)):1} = "K" ]]; then
+    ((res=${input:0:((${#input}-1))}-273))
+     echo $res"C"
+fi
